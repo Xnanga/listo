5c51c2666950519cde860a0dbc97bcce298dd9b8
Xnanga <Jpeutherer1@hotmail.co.uk>
2021-02-21 19:07:54 +0000

    Basic task creation and editing added alongside styling improvements.

--- js/script.js
+++ js/script.js
@@ -14,53 +14,142 @@ class App {
   allTaskLists = [];
   allTasks = [];
 
+  taskEditingEnabled = true;
+  taskIsBeingEdited = false;
+  taskIsFinishedEditing = true;
+
+  activeTaskObj;
+  activeTaskHTML;
+
   constructor() {
-    newTaskListBtn.addEventListener("click", this.newTaskList);
     this._attachEventListeners();
   }
 
   newTaskList() {
     const newTaskList = new TaskList();
     ui._displayNewTaskList(newTaskList);
-    app._attachEventListeners(newTaskList);
   }
 
   newTask(e) {
-    const clickedElement = e;
-    if (clickedElement.target.className !== "new-task") return;
+    const target = e.target;
+
+    if (target.className !== "new-task") return;
+    if (target.dataset.edited === "true")
+      return this.editTaskTextContent(target);
+
+    this._toggleTaskEditing();
 
     const newTask = new Task();
+    this.activeTaskObj = newTask;
+    this.activeTaskHTML = e.target;
+
+    // Change data edited to true
+    this._manageTaskId(e.target);
 
     // Toggle text input
-    // Process text input once clicked away
-    // Add it to task text content
-    // Display new add task button under
+    ui._toggleTaskTextInput(target);
 
-    ui._displayNewTask(clickedElement.target);
-    app._attachEventListeners(newTask);
+    // Display new task
+    ui._displayNewTask(target);
   }
 
-  _attachEventListeners(component) {
-    const allTaskLists = document.querySelectorAll(".task-list");
-
-    allTaskLists.forEach((list) =>
-      list.addEventListener("click", this.newTask)
+  editTaskTextContent(task) {
+    const currentTask = this.allTasks.find(
+      (storedTask) => storedTask.id === task.dataset.id
     );
+    this._processTaskTextInput();
+  }
+
+  _attachEventListeners() {
+    taskBoard.addEventListener("click", this._clickHandler.bind(this));
+    taskBoard.addEventListener("keypress", this._keyPressHandler.bind(this));
+  }
+
+  _clickHandler(e) {
+    if (e.target.classList.contains("task-board")) {
+      if (this.taskIsBeingEdited) {
+        this._handleTaskEdit();
+      }
+    }
+
+    if (!this.taskEditingEnabled) return;
+    this._handleNewTaskCreation(e);
+  }
+
+  _keyPressHandler(e) {
+    if (e.key !== "Enter") return;
+
+    if (this.taskIsBeingEdited) {
+      this._handleTaskEdit();
+    }
+
+    if (!this.taskEditingEnabled) return;
+    this._handleNewTaskCreation(e);
+  }
+
+  _processTaskTextInput(target) {
+    const taskText = this.activeTaskHTML.querySelector(".new-task__text-area")
+      .textContent;
+
+    if (!taskText) return this._undoTaskCreation(this.activeTaskHTML);
+
+    this.activeTaskObj.textContent = taskText;
+    this.activeTaskObj.node = this.activeTaskHTML;
+
+    ui._toggleTaskTextInput(this.activeTaskHTML);
+    ui._populateTextContent(this.activeTaskObj);
+  }
+
+  _toggleTaskEditing() {
+    this.taskEditingEnabled = !this.taskEditingEnabled;
+    this.taskIsBeingEdited = !this.taskIsBeingEdited;
+    this.taskIsFinishedEditing = !this.taskIsFinishedEditing;
+  }
+
+  _manageTaskId(task) {
+    task.dataset.edited = "true";
+  }
+
+  _undoTaskCreation(target) {
+    const removedTaskId = target.getAttribute("data-id");
+    const removedTaskHTML = document.querySelector(
+      `[data-id="${removedTaskId}"]`
+    );
+
+    removedTaskHTML.outerHTML = "";
+    this.allTasks.pop();
+  }
+
+  _handleTaskEdit() {
+    this._toggleTaskEditing();
+    this._processTaskTextInput();
+  }
+
+  _handleNewTaskCreation(e) {
+    if (e.target.classList.contains("new-task")) this.newTask(e);
+    if (e.target.classList.contains("new-task-list")) this.newTaskList(e);
   }
 }
 
 class UI {
   constructor() {}
 
+  _toggleTaskTextInput(task) {
+    task.querySelector(".new-task__text").classList.toggle("hidden");
+    task.querySelector(".new-task__text-area").classList.toggle("hidden");
+  }
+
   _displayNewTaskList() {
     const html = `
     <div class="task-list">
     <div class="task-list__topbar">
-      <h2 class="task-list__topbar__heading">Sgaia Foods</h2>
+      <h2 class="task-list__topbar__heading">New Task List</h2>
       <button class="task-list__topbar__btn">EDIT</button>
     </div>
-    <div class="new-task">
-      <span class="task-text">Add a new task</span>
+    <div data-edited="false" class="new-task">
+    <span class="new-task__text">Add a new task</span>
+    <div class="new-task__text-area hidden" contenteditable="true"></div>
+  </div>
     </div>
   </div>
     `;
@@ -70,22 +159,24 @@ class UI {
 
   _displayNewTask(el) {
     const html = `
-    <div class="new-task">
+    <div data-edited="false" data-id="${app.activeTaskObj.id}" class="new-task">
     <span class="new-task__text">Add a new task</span>
-    <textarea
-      class="new-task__text-area hidden"
-      name="TaskTextInput"
-      id="TaskTextInput-1"
-    ></textarea>
-    </div>
+    <div class="new-task__text-area hidden" contenteditable="true"></div>
+  </div>
     `;
 
     el.closest(".task-list").insertAdjacentHTML("beforeend", html);
+  }
+
+  _populateTextContent(task) {
+    task.node.querySelector(".new-task__text").textContent = task.textContent;
+    task.node.querySelector(".new-task__text-area").value = task.textContent;
   }
 }
 
 class TaskList {
   id = (Date.now() + "").slice(-10);
+  node;
 
   constructor(textContent) {
     this.textContent = textContent;
@@ -94,11 +185,12 @@ class TaskList {
   }
 }
 
-class Task extends TaskList {
+class Task {
   id = (Date.now() + "").slice(-10);
+  node;
 
   constructor(textContent) {
-    super(textContent);
+    this.textContent = textContent;
 
     app.allTasks.push(this);
   }
@@ -106,3 +198,5 @@ class Task extends TaskList {
 
 const app = new App();
 const ui = new UI();
+const taskList = new TaskList();
+const task = new Task();
