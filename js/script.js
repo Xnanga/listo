ea5c411e04878547747ddde4cc53d3095251aef0
Xnanga <Jpeutherer1@hotmail.co.uk>
2021-02-22 23:05:26 +0000

    Synced HTML and object id values, new task lists now auto-create a new task.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,83 @@ const newTaskListBtn = document.querySelector(".new-task-list");
 
 // Classes
 
+class Task {
+  id = (Date.now() + "").slice(-10);
+  node;
+
+  constructor(textContent, id) {
+    this.textContent = textContent;
+  }
+}
+
+class TaskList {
+  id = (Date.now() + "").slice(-10);
+  node;
+
+  constructor(textContent) {
+    const task = new Task();
+    this.textContent = textContent;
+  }
+}
+
+class UI {
+  constructor() {}
+
+  _toggleTaskTextInput(task) {
+    const newTaskText = task.querySelector(".new-task__text");
+    const newTaskTextInput = task.querySelector(".new-task__text-area");
+
+    newTaskText.classList.toggle("hidden");
+    newTaskTextInput.classList.toggle("hidden");
+
+    if (!newTaskTextInput.classList.contains("hidden")) {
+      newTaskTextInput.focus();
+    }
+  }
+
+  _displayNewTaskList() {
+    const html = `
+    <div class="task-list">
+    <div class="task-list__topbar">
+      <h2 class="task-list__topbar__heading">New Task List</h2>
+      <button class="task-list__topbar__btn">EDIT</button>
+    </div>
+    </div>
+    `;
+
+    newTaskListBtn.insertAdjacentHTML("beforebegin", html);
+
+    const allLists = document.querySelectorAll(".task-list");
+    const newestList = allLists[allLists.length - 1];
+    return newestList;
+  }
+
+  _displayNewTask(el) {
+    const html = `
+    <div data-edited="false" class="new-task">
+    <span class="new-task__text">Add a new task</span>
+    <div class="new-task__text-area hidden" contenteditable="true"></div>
+  </div>
+    `;
+
+    el.closest(".task-list").insertAdjacentHTML("beforeend", html);
+    const allTasks = el.querySelectorAll(".new-task");
+    return allTasks[allTasks.length - 1];
+  }
+
+  _populateTextContent(task) {
+    task.node.querySelector(".new-task__text").textContent = task.textContent;
+    task.node.querySelector(".new-task__text-area").value = task.textContent;
+  }
+
+  _horizontalScroll() {
+    taskBoard.scroll({
+      left: 10000,
+      behavior: "smooth",
+    });
+  }
+}
+
 class App {
   allTaskLists = [];
   allTasks = [];
@@ -21,35 +98,55 @@ class App {
   activeTaskObj;
   activeTaskHTML;
 
+  activeTaskListObj;
+  activeTaskListHTML;
+
   constructor() {
     this._attachEventListeners();
   }
 
   newTaskList() {
     const newTaskList = new TaskList();
-    ui._displayNewTaskList(newTaskList);
+    this.allTaskLists.push(newTaskList);
+    const newestTaskList = ui._displayNewTaskList(newTaskList);
+
+    this.activeTaskListObj = newTaskList;
+    this.activeTaskListHTML = newestTaskList;
+
+    this.newTaskFromList(this.activeTaskListHTML);
+
+    ui._horizontalScroll();
+  }
+
+  newTaskFromList(currentTaskList) {
+    const newTask = new Task();
+    this.allTasks.push(newTask);
+    const newestTask = ui._displayNewTask(currentTaskList);
+
+    this.activeTaskObj = newTask;
+    this.activeTaskHTML = newestTask;
   }
 
   newTask(e) {
     const target = e.target;
 
     if (target.className !== "new-task") return;
-    if (target.dataset.edited === "true")
+    if (target.dataset.edited === "true") {
       return this.editTaskTextContent(target);
+    }
 
     this._toggleTaskEditing();
 
     const newTask = new Task();
+    this.allTasks.push(newTask);
     this.activeTaskObj = newTask;
     this.activeTaskHTML = e.target;
 
-    // Change data edited to true
-    this._manageTaskId(e.target);
+    this._manageTaskId(target);
+    this._manageTaskEdited(target);
 
-    // Toggle text input
     ui._toggleTaskTextInput(target);
 
-    // Display new task
     ui._displayNewTask(target);
   }
 
@@ -66,6 +163,10 @@ class App {
   }
 
   _clickHandler(e) {
+    // Display a black overlay across the screen (Enable pointer events)
+    // Only editable task is above black overlay in z-index
+    // Trigger finished editing or undo creation when black overlay clicked
+
     if (e.target.classList.contains("task-board")) {
       if (this.taskIsBeingEdited) {
         this._handleTaskEdit();
@@ -107,7 +208,12 @@ class App {
   }
 
   _manageTaskId(task) {
+    task.dataset.id = this.activeTaskObj.id;
+  }
+
+  _manageTaskEdited(task) {
     task.dataset.edited = "true";
+    task.classList.add("new-task--populated");
   }
 
   _undoTaskCreation(target) {
@@ -129,74 +235,26 @@ class App {
     if (e.target.classList.contains("new-task")) this.newTask(e);
     if (e.target.classList.contains("new-task-list")) this.newTaskList(e);
   }
-}
 
-class UI {
-  constructor() {}
-
-  _toggleTaskTextInput(task) {
-    task.querySelector(".new-task__text").classList.toggle("hidden");
-    task.querySelector(".new-task__text-area").classList.toggle("hidden");
+  _setLocalStorage() {
+    localStorage.setItem("taskLists", JSON.stringify(this.allTaskLists));
+    localStorage.setItem("tasks", JSON.stringify(this.allTasks));
   }
 
-  _displayNewTaskList() {
-    const html = `
-    <div class="task-list">
-    <div class="task-list__topbar">
-      <h2 class="task-list__topbar__heading">New Task List</h2>
-      <button class="task-list__topbar__btn">EDIT</button>
-    </div>
-    <div data-edited="false" class="new-task">
-    <span class="new-task__text">Add a new task</span>
-    <div class="new-task__text-area hidden" contenteditable="true"></div>
-  </div>
-    </div>
-  </div>
-    `;
+  _getLocalStorage() {
+    const taskListData = JSON.parse(localStorage.getItem("taskLists"));
+    const taskData = JSON.parse(localStorage.getItem("tasks"));
 
-    newTaskListBtn.insertAdjacentHTML("beforebegin", html);
-  }
+    if (!taskListData || !taskData) return;
 
-  _displayNewTask(el) {
-    const html = `
-    <div data-edited="false" data-id="${app.activeTaskObj.id}" class="new-task">
-    <span class="new-task__text">Add a new task</span>
-    <div class="new-task__text-area hidden" contenteditable="true"></div>
-  </div>
-    `;
+    this.allTaskLists = taskListData;
+    this.allTasks = taskData;
 
-    el.closest(".task-list").insertAdjacentHTML("beforeend", html);
-  }
+    // Add logic for iterating through all taskLists and rendering each in order
 
-  _populateTextContent(task) {
-    task.node.querySelector(".new-task__text").textContent = task.textContent;
-    task.node.querySelector(".new-task__text-area").value = task.textContent;
-  }
-}
-
-class TaskList {
-  id = (Date.now() + "").slice(-10);
-  node;
-
-  constructor(textContent) {
-    this.textContent = textContent;
-
-    app.allTaskLists.push(this);
-  }
-}
-
-class Task {
-  id = (Date.now() + "").slice(-10);
-  node;
-
-  constructor(textContent) {
-    this.textContent = textContent;
-
-    app.allTasks.push(this);
+    // Add logic for iterating through all tasks and rendering each in their respective tasklists
   }
 }
 
 const app = new App();
 const ui = new UI();
-const taskList = new TaskList();
-const task = new Task();
